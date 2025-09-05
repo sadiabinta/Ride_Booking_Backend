f6a121420b5ca0b25a1a029b4a80ceeb917b87b3
sadiabinta <sadiaa.kabir@gmail.com>
2025-09-05 10:23:02 -0400

    server done

---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/modules/config/env";
+
+let server: Server;
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("Connected to DB");
+    server = app.listen(5000, () => {
+      console.log("Server is listening to port 5000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
+
+process.on("unhandledRejection", () => {
+  console.log("Unhandled Rejection detected...server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("uncaughtException", () => {
+  console.log("Uncaught Rejection detected...server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received...server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
